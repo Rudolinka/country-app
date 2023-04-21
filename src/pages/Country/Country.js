2b8ec7a0e169774e3acfa9fb16bab25de16f67eb
Rudolinka <daria_piotrowska@interia.pl>
2023-04-21 20:46:44 +0200

    added styles to country.js

--- src/pages/Country/Country.js
+++ src/pages/Country/Country.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Country.module.css";
 
 const Country = () => {
   const [current, setCurrent] = useState();
@@ -17,6 +18,22 @@ const Country = () => {
   console.log(current);
   if (!current) return "Loading...";
 
-  return <div>{current.name.official}</div>;
+  const currencies = Object.entries(current.currencies).map((v) => v[0]);
+  const languages = Object.keys(current.languages);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.box}>
+        <h1>{current.name.official}</h1>
+        <img src={current.coatOfArms.png} alt={current.coatOfArms.alt} />
+        <p>Capital: {current.capital}</p>
+        <p>👤 {current.population} people</p>
+        <p>Size: {current.area} m2</p>
+        <p>Subregion: {current.subregion}</p>
+        <p>Main currency: {current.currencies[currencies[0]].name}</p>
+        <p>Main language: {current.languages[languages[0]]}</p>
+      </div>
+    </div>
+  );
 };
 export default Country;
