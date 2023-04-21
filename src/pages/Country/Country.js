d90518a167ffc25f99b39657d8f665a8a4e77396
Rudolinka <daria_piotrowska@interia.pl>
2023-04-21 19:49:10 +0200

    fetch country by name

--- src/pages/Country/Country.js
+++ src/pages/Country/Country.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Country = () => {
   const [current, setCurrent] = useState();
+  const { name } = useParams();
 
   useEffect(() => {
     getCountryByName();
   }, []);
 
   const getCountryByName = async () => {
-    const response = await fetch(`https://restcountries.com/v3.1/name/Poland`);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
     const data = await response.json();
     setCurrent(data[0]);
   };
   console.log(current);
+  if (!current) return "Loading...";
+
   return <div>{current.name.official}</div>;
 };
 export default Country;
